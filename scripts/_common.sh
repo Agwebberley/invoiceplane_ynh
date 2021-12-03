e52d2553d792e761c452e509a90b442ef869c0d6
Andrew Webberley <44350096+Agwebberley@users.noreply.github.com>
2021-12-03 08:33:08 -0800

    manifest stuff and dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 phpversion=7.4
 # dependencies used by the app
-pkg_dependencies="php${phpversion}-fpm php${phpversion}-gd php${phpversion}-hash php${phpversion}-json php${phpversion}-mbstring php${phpversion}-mcrypt php${phpversion}-mysqli php${phpversion}-openssl php${phpversion}-recode php${phpversion}-xmlrpc php${phpversion}-zlib"
+pkg_dependencies="php${phpversion}-fpm php${phpversion}-gd php${phpversion}-mbstring php${phpversion}-mysqli openssl php${phpversion}-xmlrpc"
 
 #=================================================
 # PERSONAL HELPERS
